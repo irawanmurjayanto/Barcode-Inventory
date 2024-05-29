0c1de44a8b6cc07e89b984eb65464764516ed50f
irawanmurjayanto <162110432+irawanmurjayanto@users.noreply.github.com>
2024-05-29 15:33:26 +0700

    Update AppConfig.java

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,8 +1,6 @@
 package com.example.barcodegcnew;
 
 public class AppConfig {
-
-  //  public static final String IP_SERVER = "http://golden-care.co.id/android";
-  //public static final String IP_SERVER = "http://kinipi.net/";
-  public static final String IP_SERVER = "http://kinipi.net/";
+ 
+  public static final String IP_SERVER = "http://server.com/";
 }
